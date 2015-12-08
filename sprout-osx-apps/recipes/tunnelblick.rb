53582e0da85750cc65a87decbcb6769b04482f9a
Joao C Costa <joaocc@users.noreply.github.com>
2015-12-08 02:02:08 +0000

    Update Tunnelblick to 3.5.5_build_4270.4461.dmg

--- sprout-osx-apps/recipes/tunnelblick.rb
+++ sprout-osx-apps/recipes/tunnelblick.rb
@@ -1,7 +1,7 @@
 dmg_package "Tunnelblick" do
-  dmg_name "Tunnelblick_3.2.8.dmg"
-  source "http://tunnelblick.googlecode.com/files/Tunnelblick_3.2.8.dmg"
-  checksum "e3d6ba702b6bc58a1a470d2fcaea95b7ead8e26965820cba2b919d58e9190b78"
+  dmg_name "Tunnelblick_3.5.5_build_4270.4461.dmg"
+  source "https://tunnelblick.net/release/Tunnelblick_3.5.5_build_4270.4461.dmg"
+  checksum "0977d4f35706ed5000ff6f9ab11471ce0eaf9acc6319b6d6157191894c4038d5"
   action :install
   owner node['current_user']
 end
